bfd63818125dbe859ecba27765436a2eb1a9bdf8
MicahMartin <micah.m329@gmail.com>
2019-01-23 17:14:46 -0500

    .

--- src/bitflip.c
+++ src/bitflip.c
@@ -14,7 +14,7 @@ int main (void){
   // 1 is ...00000001 in binary. Shifting it by 5 will make it 00100000.
 
   /* set 4th bit of DDRB to 0 for the corresponding pin to be used as input*/
-  DDRB &= ~(1 << 4);
+  //DDRB &= ~(1 << 4);
   // ~(1<<x) is good to keep in mind. It will first create a bitmask of 0001 0000
   // then invert it with the notter (~) to get a mask of 11101111
   // So the comparison wont change a bit if its true, because 1 & 1 is 1, and it wont change a bit if its 0, because 0 & 1 is 0. so the only bit with potential to change is the false bit, since 1 & 0 is false.
@@ -24,7 +24,7 @@ int main (void){
   // This will ensure that pin4 is on input. 
 
   /*set 4th bit of PORTB to high so we can rely on internal pull up resistor*/ 
-  PORTB |= (1 << 4);
+  //PORTB |= (1 << 4);
 
   while(1) {
     //This conditional checks to see if the 4th bit in PINB is 0 or 1.
@@ -34,15 +34,15 @@ int main (void){
 
     // tl;dr 0001 0000 is truthy
     // note: pinx register is read only. it gets the state of the corresponding pin
-    if (!(PINB & (1<<4))) {
+    //if (!(PINB & (1<<4))) {
       //Button is pressed, do stuff.
       
-    }
+    //}
     /* toggle 5th bit high to low to turn led connected to the corresponding pin on and off */
-    // PORTB ^= 1 << 5;
+    PORTB ^= 1 << 5;
     // 0 xor 1 = 1
     // 1 xor 1 = 0
     // Toggles
-    //_delay_ms(MY_DELAY);
+    _delay_ms(MY_DELAY);
    }
 }
